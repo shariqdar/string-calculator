e7c9e274b49c5738e6903ae075cc6bd9f8cc09e7
Shariq Dar <shariqdar@Shariqs-MacBook-Pro.local>
2024-07-10 20:19:09 +0530

    added some test cases

---
+++ test.js
@@ -0,0 +1,22 @@
+const StringCalculator = require('./index.js').StringCalculator;
+const expect = require('chai').expect;
+
+describe('String Calculator', function() {
+    it('1) Handle new lines between numbers', function(done){
+        let c1 = new StringCalculator("1\n2,3");
+        expect(c1.calculate()).to.equal(6);
+        done();
+    });
+
+    it('2) Support different delimiters', function(done) {
+        let c2 = new StringCalculator("//;\n1;2");
+        expect(c2.calculate()).to.equal(3);
+        done();
+    });
+
+    it('3) Negative Numbers', function(done) {
+        let c3 = new StringCalculator("//;\n-1;-2");
+        expect(c3.calculate()).to.throw('negative numbers not allowed -1, -2');
+        done();
+    });
+});
